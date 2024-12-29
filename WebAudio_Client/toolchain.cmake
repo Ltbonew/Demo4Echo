35e1accba2faa89f902eb982e589b0f0f7040f80
kingham <1425962791@qq.com>
2024-12-29 10:31:07 +0800

    add Cross compile for ARM(echo-rv1106)

--- WebAudio_Client/toolchain.cmake
+++ WebAudio_Client/toolchain.cmake
@@ -2,12 +2,14 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
+set(SDK_PATH /path/to/sdk)
+
 # Specify the compiler paths
-set(CMAKE_C_COMPILER /path/to/arm-gcc)
-set(CMAKE_CXX_COMPILER /path/to/arm-g++)
+set(CMAKE_C_COMPILER ${SDK_PATH}/tools/linux/toolchain/arm-rockchip830-linux-uclibcgnueabihf/bin/arm-rockchip830-linux-uclibcgnueabihf-gcc)
+set(CMAKE_CXX_COMPILER ${SDK_PATH}/tools/linux/toolchain/arm-rockchip830-linux-uclibcgnueabihf/bin/arm-rockchip830-linux-uclibcgnueabihf-g++)
 
 # Specify the sysroot (if available)
-set(CMAKE_SYSROOT /path/to/sysroot)
+set(CMAKE_SYSROOT ${SDK_PATH}/sysdrv/source/buildroot/buildroot-2023.02.6/output/host/arm-buildroot-linux-uclibcgnueabihf/sysroot)
 
 # Add paths to find libraries and includes
 set(CMAKE_FIND_ROOT_PATH ${CMAKE_SYSROOT})
