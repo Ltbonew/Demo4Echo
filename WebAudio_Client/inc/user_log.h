bbd73e437a6fa1b23296f49b4eb80b6649470337
kingham <1425962791@qq.com>
2025-01-03 20:05:52 +0800

    update user log

--- WebAudio_Client/inc/user_log.h
+++ WebAudio_Client/inc/user_log.h
@@ -1,12 +1,9 @@
 #ifndef USER_LOG_H
 #define USER_LOG_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 #include <stdio.h>
 #include <time.h>
+#include <libgen.h>  // For basename()
 
 // 获取当前时间戳
 static inline void get_timestamp(char *buf, size_t len) {
@@ -18,22 +15,24 @@ static inline void get_timestamp(char *buf, size_t len) {
     strftime(buf, len, "%H:%M:%S", time_info);
 }
 
+// 提取文件名（不带路径）
+static inline const char* get_filename(const char *path) {
+    char *filename = basename((char *)path);
+    return filename ? filename : "unknown";
+}
+
 // 定义日志宏
 #define USER_LOG(level, format, ...) \
     do { \
         char timestamp[9]; \
         get_timestamp(timestamp, sizeof(timestamp)); \
         fprintf(stderr, "[%s] (%s) %s: " format " %s:%d\n", \
-                level, timestamp, __func__, ##__VA_ARGS__, __FILE__, __LINE__); \
+                level, timestamp, __func__, ##__VA_ARGS__, get_filename(__FILE__), __LINE__); \
     } while (0)
 
 // 具体的日志级别宏定义
 #define USER_LOG_INFO(format, ...)   USER_LOG("INFO", format, ##__VA_ARGS__)
 #define USER_LOG_WARN(format, ...)   USER_LOG("WARN", format, ##__VA_ARGS__)
-#define USER_LOG_ERRO(format, ...)  USER_LOG("ERRO", format, ##__VA_ARGS__)
-
-#ifdef __cplusplus
-} /*extern "C"*/
-#endif
+#define USER_LOG_ERROR(format, ...)  USER_LOG("ERRO", format, ##__VA_ARGS__)
 
 #endif // USER_LOG_H
