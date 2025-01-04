cc54251df493c79f21279267c498c1d3bd55f4b3
kingham <1425962791@qq.com>
2025-01-04 15:14:35 +0800

    update page manager test

--- lvgl_demo/main.c
+++ lvgl_demo/main.c
@@ -82,13 +82,27 @@ void page1_deinit(void)
     return;
 }
 
+static void btn2_event_handler(lv_event_t * e)
+{
+    lv_event_code_t code = lv_event_get_code(e);
+
+    if(code == LV_EVENT_CLICKED) {
+        LV_LOG_USER("Clicked");
+        lv_lib_pm_OpenPrePage(&page_manager);
+    }
+    else if(code == LV_EVENT_VALUE_CHANGED) {
+        LV_LOG_USER("Toggled");
+    }
+}
+
 void page2_init(void)
 {
     lv_obj_t *ui_Page2 = lv_obj_create(NULL);
-    lv_obj_t * btn1 = lv_btn_create(ui_Page2);
-    lv_obj_align(btn1, LV_ALIGN_CENTER, 0, -40);
+    lv_obj_t * btn2 = lv_btn_create(ui_Page2);
+    lv_obj_add_event_cb(btn2, btn2_event_handler, LV_EVENT_ALL, NULL);
+    lv_obj_align(btn2, LV_ALIGN_CENTER, 0, -40);
 
-    lv_obj_t* label = lv_label_create(btn1);
+    lv_obj_t* label = lv_label_create(btn2);
     lv_label_set_text(label, "Button2");
     lv_obj_center(label);
 
@@ -103,6 +117,10 @@ void page2_deinit(void)
 
 void lv_demo_test()
 {   
+    lv_disp_t * dispp = lv_disp_get_default();
+    lv_theme_t * theme = lv_theme_default_init(dispp, lv_palette_main(LV_PALETTE_BLUE), lv_palette_main(LV_PALETTE_RED),
+                                               true, LV_FONT_DEFAULT);
+    lv_disp_set_theme(dispp, theme);
     
     lv_lib_pm_Init(&page_manager);
     lv_lib_pm_page_t *pm_page1 = lv_lib_pm_CreatePage(&page_manager, "Page1", page1_init, page1_deinit, NULL);
