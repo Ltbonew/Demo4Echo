73d35cff192f95f5f9e119c21a557473a4d1a4b9
kingham <1425962791@qq.com>
2025-01-27 11:07:55 +0800

    fix audio buffer proc bug

--- WebAudio_Server/MessageHandler.py
+++ WebAudio_Server/MessageHandler.py
@@ -1,4 +1,3 @@
-
 import json
 import logging
 from collections import deque
@@ -12,11 +11,12 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 logger = logging.getLogger(__name__)
 
 class MessageHandler:
-    def __init__(self):
+    def __init__(self, protocol_version="1"):
+        self.protocol_version = protocol_version
         # 缓冲区设置
-        self.BUFFER_MAX_LENGTH_MS = 15 * 1000  # 缓冲区最大长度
+        self.BUFFER_MAX_LENGTH_MS = 10 * 1000  # 缓冲区最大长度
         self.VAD_FRAME_DURATION_MS = 200  # VAD 推理的片段长度
-        self.NO_SPEECH_TIMEOUT_MS = 2000  # 无语音活动超时
+        self.NO_SPEECH_TIMEOUT_MS = 3000  # 无语音活动超时
         self.POST_SPEECH_BUFFER_MS = 200  # 语音结束后的判断结束的时间
         # 初始化音频处理器
         self.audio_processor = AudioProcessor()
@@ -41,31 +41,26 @@ class MessageHandler:
         self.start_speech_pos = -1  # 毫秒
         self.last_speech_pos = -1  # 毫秒
         self.model_manager.VAD_cache_clean()
+        logger.info("Audio processor reset")
 
-    # 处理音频数据
-    async def handle_audio_data(self, payload):
-        # 解码音频数据
-        pcm_data = self.audio_processor.decode_audio(payload)
-        audio_data_array = np.frombuffer(pcm_data, dtype=np.int16)
-        # 将解码后的音频数据添加到缓冲区
-        self.audio_buffer = np.append(self.audio_buffer, audio_data_array)
+    def VAD_proc_audio_stream(self):
         # 计算音频数据的时长
         audio_length = len(self.audio_buffer) * 1000 // self.SAMPLE_RATE
-
-        # 使用 VAD 推理，处理200ms片段
-        chunk_size = self.SAMPLE_RATE * self.VAD_FRAME_DURATION_MS // 1000
-
+        # 使用 VAD 推理，处理200ms片段对应数据长度
+        chunk_stride = self.SAMPLE_RATE * self.VAD_FRAME_DURATION_MS // 1000
+        # 不足200ms的音频数据，不进行VAD处理
         if audio_length - self.vad_process_pos < self.VAD_FRAME_DURATION_MS:
-            return None # 不足200ms的音频数据，不进行VAD处理
-
-        # 获取最近的200ms音频片段
-        speech_chunk = self.audio_buffer[-chunk_size*self.SAMPLE_RATE//1000:]
-
+            return None
+        # 获取下一个的200ms音频片段
+        # speech_chunk = self.audio_buffer[-chunk_size*self.SAMPLE_RATE//1000:]
+        beg_frame = self.vad_process_pos*self.SAMPLE_RATE//1000
+        end_frame = (self.vad_process_pos)*self.SAMPLE_RATE//1000 + chunk_stride
+        speech_chunk = self.audio_buffer[beg_frame : end_frame]
         # VAD推理
         start, end = self.model_manager.VAD_Detection(speech_chunk)
         # 更新VAD处理的位置
-        self.vad_process_pos += chunk_size  # 处理了一个200ms片段
-
+        self.vad_process_pos += self.VAD_FRAME_DURATION_MS  # 处理了一个200ms片段
+        logger.info("start: %s, end: %s", start, end)
         # 处理VAD结果, 判定当前状态
         if end is not None:
             self.start_speech_pos = 0
@@ -79,48 +74,51 @@ class MessageHandler:
         # 如果一直在说话, last_speech_pos那就是当前的音频长度
         if self.is_speaking:
             self.last_speech_pos = audio_length
-
         # 如果超过等待说话时间3s都一直都没有说话
         if self.start_speech_pos == -1:
             if audio_length > self.NO_SPEECH_TIMEOUT_MS:
                 logger.info("No speech detected for %d seconds, drop...", self.NO_SPEECH_TIMEOUT_MS // 1000)
-                self.audio_proc_reset()
-                response = {
+                res = {
                     "type": "vad",
                     "state": "no_speech"
                 }
-                return response
+                return res
 
-        # 判断是否达到15s的缓冲区最大长度，若达到，则触发ASR识别
+        # 判断是否达到10s的缓冲区最大长度，若达到，则触发ASR识别
         if audio_length > self.BUFFER_MAX_LENGTH_MS:
-            logger.info("Buffer exceeded 15 seconds, trigger ASR processing...")
-            asr_text = self.model_manager.ASR_generate_text(self.audio_buffer)
-            logger.info(f"ASR result: {asr_text}")
-            self.audio_proc_reset()
-            response = {
-                "type": "asr",
-                "text": asr_text
+            logger.info("Buffer exceeded %d seconds", self.BUFFER_MAX_LENGTH_MS // 1000)
+            res = {
+                "type": "vad",
+                "state": "too_long"
             }
-            return response
+            return res
 
         # 判断是否超过1s未检测到语音活动，若超过则触发ASR识别
         if self.last_speech_pos > 0 and (audio_length - self.last_speech_pos) > self.POST_SPEECH_BUFFER_MS:
             logger.info("end speech position: %d", self.last_speech_pos)
-            logger.info("Post-speech buffer exceeded 1 second, trigger ASR processing...")
-            asr_text = self.model_manager.ASR_generate_text(self.audio_buffer[:self.last_speech_pos*self.SAMPLE_RATE//1000].astype(np.float32))
-            logger.info(f"ASR result: {asr_text}")
-            self.audio_proc_reset()
-            response = {
-                "type": "asr",
-                "text": asr_text
+            logger.info("Post-speech buffer exceeded 1 second")
+            res = {
+                "type": "vad",
+                "state": "end"
             }
-            return response
-
+            return res
         return None
+
+    # 处理音频数据
+    async def handle_audio_data(self, payload):
+        # 解码音频数据
+        pcm_data = self.audio_processor.decode_audio(payload)
+        audio_data_array = np.frombuffer(pcm_data, dtype=np.int16)
+        # 将解码后的音频数据添加到缓冲区
+        self.audio_buffer = np.append(self.audio_buffer, audio_data_array)
+        # 处理音频流
+        res = self.VAD_proc_audio_stream()
+        return res
 
     # 处理文本数据
     async def handle_text_message(self, data):
         if data.get('type') == 'hello':
+            self.audio_proc_reset()
             audio_params = data.get('audio_params', {})
             # 暂时没设定可变的音频参数列表, 所以client发送过来的音频参数不会被使用
             # sample_rate = audio_params.get('sample_rate', AudioProcessor.SAMPLE_RATE)
@@ -129,20 +127,33 @@ class MessageHandler:
             # logger.info(f"Set audio parameters: sample_rate={sample_rate}, channels={channels}, frame_duration_ms={frame_duration_ms}")
             # self.audio_processor.set_audio_params(sample_rate, channels, frame_duration_ms)
 
-        # 处理消息并发送响应
-        response = {
-            "type": "reply",
-            "message": "OK"
-        }
-        return response
+        if data.get('type') == 'state' :
+            if data.get('state') == 'idle':
+                self.audio_proc_reset()
 
+        if data.get('type') == 'state' :
+            if data.get('state') == 'listening':
+                self.audio_proc_reset()
+
+            elif data.get('state') == 'thinking':
+                asr_text = self.model_manager.ASR_generate_text(self.audio_buffer[:self.last_speech_pos*self.SAMPLE_RATE//1000].astype(np.float32))
+                if asr_text:
+                    logger.info(f"ASR result: {asr_text}")
+                    # asr识别异常处理还需要
+                    response =  {
+                        "type": "asr",
+                        "text": asr_text
+                    }
+                    self.audio_proc_reset()
+                    return response
+        return None
     # 处理所有消息
     async def handle_message(self, message):
         if isinstance(message, bytes):  # 如果消息是二进制数据
             bin_protocol = self.audio_processor.unpack_bin_frame(message)
             if bin_protocol:
                 version, type, payload = bin_protocol
-                if str(version) == WebsocketServer.PROTOCOL_VERSION and type == 0:
+                if str(version) == self.protocol_version and type == 0:
                     # 处理音频数据
                     response = await self.handle_audio_data(payload)
                     if response is not None:
