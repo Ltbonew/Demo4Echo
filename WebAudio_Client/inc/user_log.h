ec51920119afcde82f217d2f6b610a2d6f0aef5e
kingham <1425962791@qq.com>
2025-02-07 19:06:28 +0800

    fix log way(can not show filename on arm linux board)

--- WebAudio_Client/inc/user_log.h
+++ WebAudio_Client/inc/user_log.h
@@ -5,11 +5,6 @@
 #include <time.h>
 #include <libgen.h>  // For basename()
 
-// ANSI颜色代码
-#define COLOR_RESET   "\033[0m"
-#define COLOR_RED     "\033[31m"
-#define COLOR_YELLOW  "\033[33m"
-
 // 获取当前时间戳
 static inline void get_timestamp(char *buf, size_t len) {
     time_t raw_time;
@@ -26,18 +21,18 @@ static inline const char* get_filename(const char *path) {
     return filename ? filename : "unknown";
 }
 
-// 定义日志宏，带有颜色
-#define USER_LOG(level_color, level, format, ...) \
+// 辅助宏，用于处理可变参数
+#define USER_LOG_INTERNAL(level, format, ...) \
     do { \
         char timestamp[9]; \
         get_timestamp(timestamp, sizeof(timestamp)); \
-        fprintf(stderr, "%s[%s]%s" " (%s) %s: " format " %s:%d\n", \
-                level_color, level, COLOR_RESET, timestamp, __func__, ##__VA_ARGS__, get_filename(__FILE__), __LINE__); \
+        fprintf(stderr, "[%s] (%s) %s: " format "\n", \
+                level, timestamp, __func__, ##__VA_ARGS__); \
     } while (0)
 
-// 具体的日志级别宏定义，应用颜色
-#define USER_LOG_INFO(format, ...)   USER_LOG("", "INFO", format, ##__VA_ARGS__)
-#define USER_LOG_WARN(format, ...)   USER_LOG(COLOR_YELLOW, "WARN", format, ##__VA_ARGS__)
-#define USER_LOG_ERROR(format, ...)  USER_LOG(COLOR_RED, "ERRO", format, ##__VA_ARGS__)
+// 定义日志宏
+#define USER_LOG_INFO(format, ...) USER_LOG_INTERNAL("INFO", format, ##__VA_ARGS__)
+#define USER_LOG_WARN(format, ...) USER_LOG_INTERNAL("WARN", format, ##__VA_ARGS__)
+#define USER_LOG_ERROR(format, ...) USER_LOG_INTERNAL("ERRO", format, ##__VA_ARGS__)
 
 #endif // USER_LOG_H
