38ecb5dd71dfed9dea9c8ae8f3f8d06b3c8fc518
kingham <1425962791@qq.com>
2025-02-10 19:05:08 +0800

    fix delay

--- AI_Chat_demo/Client/src/Application.cc
+++ AI_Chat_demo/Client/src/Application.cc
@@ -269,7 +269,7 @@ void Application::Run() {
     ws_client_.Connect();
     // 必须等待连接建立
     while(!ws_client_.IsConnected()) {
-        std::this_thread::sleep_for(std::chrono::seconds(2));
+        std::this_thread::sleep_for(std::chrono::seconds(5));
         ws_client_.Connect();
     }
     
