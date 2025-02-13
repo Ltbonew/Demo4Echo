150a21ef8a857906f9aa8f9de1a4ad166a0f2df1
kingham <1425962791@qq.com>
2025-02-13 20:19:57 +0800

    update fonts and images

--- lvgl_demo/gui_app/ui.h
+++ lvgl_demo/gui_app/ui.h
@@ -11,10 +11,12 @@ extern "C" {
 LV_IMG_DECLARE(ui_img_1946976022);    // assets/天气64.png
 LV_IMG_DECLARE(ui_img_320358418);    // assets/日历64_nonum.png
 LV_IMG_DECLARE(ui_img_634722903);    // assets/备忘录64.png
+LV_IMG_DECLARE(ui_img_21600057);    // assets/记忆64.png
 LV_IMG_DECLARE(ui_img_1318505016);    // assets/日历64.png
 
 // FONTS
 LV_FONT_DECLARE(ui_font_iconfont26);
+LV_FONT_DECLARE(ui_font_iconfont36);
 LV_FONT_DECLARE(ui_font_iconfont48);
 
 // UI INIT
