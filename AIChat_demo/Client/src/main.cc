13f1693fe13b2f7b635c48581f21b736edd213b5
kingham <1425962791@qq.com>
2025-02-20 10:43:59 +0800

    update main

--- AIChat_demo/Client/src/main.cc
+++ AIChat_demo/Client/src/main.cc
@@ -26,10 +26,8 @@ int main(int argc, char* argv[]) {
         int channels = 1;
         int frame_duration = 40;
 
-        // 创建 AudioProcess 实例
-        AudioProcess audio_processor(sample_rate, channels, frame_duration);
         // 创建 Application 实例
-        Application app(address, port, token, deviceId, protocolVersion, audio_processor);
+        Application app(address, port, token, deviceId, protocolVersion, sample_rate, channels, frame_duration);
 
         // 运行应用程序
         app.Run();
