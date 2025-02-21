c6dce5c94e4b77f435d98ab1a8b6c8911a9def51
kingham <1425962791@qq.com>
2025-02-21 16:33:59 +0800

    update calendar page

--- lvgl_demo/gui_app/pages/ui_CalendarPage/ui_CalendarPage.c
+++ lvgl_demo/gui_app/pages/ui_CalendarPage/ui_CalendarPage.c
@@ -1,5 +1,5 @@
 #include "ui_CalendarPage.h"
-
+#include "app_HomePage.h"
 ///////////////////// VARIABLES ////////////////////
 
 
@@ -8,29 +8,39 @@
 
 ///////////////////// FUNCTIONS ////////////////////
 
+static void ui_enent_Gesture(lv_event_t * e)
+{
+    lv_event_code_t code = lv_event_get_code(e);
+    if(code == LV_EVENT_GESTURE)
+    {
+        if(lv_indev_get_gesture_dir(lv_indev_get_act()) == LV_DIR_LEFT || lv_indev_get_gesture_dir(lv_indev_get_act()) == LV_DIR_RIGHT)
+        {
+            lv_lib_pm_OpenPrePage(&page_manager);
+        }
+    }
+}
 
 ///////////////////// SCREEN init ////////////////////
 
 void ui_CalendarPage_init()
 {
-    lv_obj_t  * calendar = lv_calendar_create(NULL);
-    lv_obj_set_size(calendar, 300, 300);
+    lv_obj_t * ui_CalendarPage = lv_obj_create(NULL);
+    lv_obj_remove_flag(ui_CalendarPage, LV_OBJ_FLAG_SCROLLABLE);      /// Flags
+    lv_obj_t * calendar = lv_calendar_create(ui_CalendarPage);
+    lv_obj_t * ui_Calendar1_header = lv_calendar_header_arrow_create(calendar);
+    lv_obj_set_width(calendar, 320);
+    lv_obj_set_height(calendar, 240);
     lv_obj_align(calendar, LV_ALIGN_TOP_MID, 0, 0);
+    int year, month, day;
+    get_current_date(&year, &month, &day);
+    lv_calendar_set_today_date(calendar, year, month, day);
+    lv_calendar_set_showed_date(calendar, year, month);
 
-    lv_calendar_set_today_date(calendar, 2024, 03, 22);
-    lv_calendar_set_showed_date(calendar, 2024, 03);
-
-#if LV_USE_CALENDAR_HEADER_DROPDOWN
-    lv_calendar_header_dropdown_create(calendar);
-#elif LV_USE_CALENDAR_HEADER_ARROW
-    lv_calendar_header_arrow_create(calendar);
-#endif
-
-    lv_calendar_set_chinese_mode(calendar, true);
-    lv_obj_set_style_text_font(calendar, &lv_font_simsun_16_cjk, LV_PART_MAIN);
+    // event
+    lv_obj_add_event_cb(ui_CalendarPage, ui_enent_Gesture, LV_EVENT_ALL, NULL);
 
     // load page
-    lv_scr_load_anim(calendar, LV_SCR_LOAD_ANIM_MOVE_RIGHT, 100, 0, true);
+    lv_scr_load_anim(ui_CalendarPage, LV_SCR_LOAD_ANIM_MOVE_RIGHT, 100, 0, true);
 }
 
 /////////////////// SCREEN deinit ////////////////////
