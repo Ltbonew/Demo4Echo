5db9df94fc105680189e525561e8323157836755
kingham <1425962791@qq.com>
2025-02-22 16:44:43 +0800

    update animation

--- lvgl_demo/gui_app/pages/ui_ChatBotPage/ui_ChatBotPage.c
+++ lvgl_demo/gui_app/pages/ui_ChatBotPage/ui_ChatBotPage.c
@@ -19,25 +19,25 @@ static void _IdleMove1_Animation(void)
     int16_t y_pos_now = -25;
     int16_t x_pos_now = 0;
     int16_t hight_now = 80;
-    lv_lib_anim_user_animation(ui_EyesPanel, 0, 500, y_pos_now, y_pos_now-20, 0, 0, 0, 0, lv_anim_path_ease_in_out, lv_lib_anim_callback_set_y);
+    lv_lib_anim_user_animation(ui_EyesPanel, 0, 500, y_pos_now, y_pos_now-20, 0, 0, 0, 0, lv_anim_path_ease_in_out, lv_lib_anim_callback_set_y, NULL);
     y_pos_now -= 20;
-    lv_lib_anim_user_animation(ui_EyesPanel, 0, 500, x_pos_now, x_pos_now-20, 0, 0, 0, 0, lv_anim_path_ease_in_out, lv_lib_anim_callback_set_x);
+    lv_lib_anim_user_animation(ui_EyesPanel, 0, 500, x_pos_now, x_pos_now-20, 0, 0, 0, 0, lv_anim_path_ease_in_out, lv_lib_anim_callback_set_x, NULL);
     x_pos_now -= 20;
-    lv_lib_anim_user_animation(ui_EyesPanel, 1000, 100, hight_now, 10, 0, 100, 0, 2, lv_anim_path_ease_out, lv_lib_anim_callback_set_hight);
+    lv_lib_anim_user_animation(ui_EyesPanel, 1000, 100, hight_now, 10, 0, 100, 0, 2, lv_anim_path_ease_out, lv_lib_anim_callback_set_hight, NULL);
     // hight_now += 0;
-    lv_lib_anim_user_animation(ui_EyesPanel, 1500, 500, x_pos_now, x_pos_now+20, 0, 0, 0, 0, lv_anim_path_ease_in_out, lv_lib_anim_callback_set_x);
+    lv_lib_anim_user_animation(ui_EyesPanel, 1500, 500, x_pos_now, x_pos_now+20, 0, 0, 0, 0, lv_anim_path_ease_in_out, lv_lib_anim_callback_set_x, NULL);
     x_pos_now += 20;
-    lv_lib_anim_user_animation(ui_EyesPanel, 2000, 100, hight_now, 10, 0, 100, 0, 2, lv_anim_path_ease_out, lv_lib_anim_callback_set_hight);
+    lv_lib_anim_user_animation(ui_EyesPanel, 2000, 100, hight_now, 10, 0, 100, 0, 2, lv_anim_path_ease_out, lv_lib_anim_callback_set_hight, NULL);
     // hight_now += 0;
-    lv_lib_anim_user_animation(ui_EyesPanel, 3000, 500, x_pos_now, x_pos_now+40, 0, 0, 0, 0, lv_anim_path_ease_in_out, lv_lib_anim_callback_set_x);
+    lv_lib_anim_user_animation(ui_EyesPanel, 3000, 500, x_pos_now, x_pos_now+40, 0, 0, 0, 0, lv_anim_path_ease_in_out, lv_lib_anim_callback_set_x, NULL);
     x_pos_now += 40;
-    lv_lib_anim_user_animation(ui_EyesPanel, 3000, 500, y_pos_now, y_pos_now+40, 0, 0, 0, 0, lv_anim_path_ease_in_out, lv_lib_anim_callback_set_y);
+    lv_lib_anim_user_animation(ui_EyesPanel, 3000, 500, y_pos_now, y_pos_now+40, 0, 0, 0, 0, lv_anim_path_ease_in_out, lv_lib_anim_callback_set_y, NULL);
     y_pos_now += 40;
-    lv_lib_anim_user_animation(ui_EyesPanel, 4000, 100, hight_now, 10, 0, 100, 0, 2, lv_anim_path_ease_out, lv_lib_anim_callback_set_hight);
+    lv_lib_anim_user_animation(ui_EyesPanel, 4000, 100, hight_now, 10, 0, 100, 0, 2, lv_anim_path_ease_out, lv_lib_anim_callback_set_hight, NULL);
     // hight_now += 0;
-    lv_lib_anim_user_animation(ui_EyesPanel, 5000, 500, x_pos_now, x_pos_now-40, 0, 0, 0, 0, lv_anim_path_ease_in_out, lv_lib_anim_callback_set_x);
+    lv_lib_anim_user_animation(ui_EyesPanel, 5000, 500, x_pos_now, x_pos_now-40, 0, 0, 0, 0, lv_anim_path_ease_in_out, lv_lib_anim_callback_set_x, NULL);
     x_pos_now -= 40;
-    lv_lib_anim_user_animation(ui_EyesPanel, 5000, 500, y_pos_now, y_pos_now-20, 0, 0, 0, 0, lv_anim_path_ease_in_out, lv_lib_anim_callback_set_y);
+    lv_lib_anim_user_animation(ui_EyesPanel, 5000, 500, y_pos_now, y_pos_now-20, 0, 0, 0, 0, lv_anim_path_ease_in_out, lv_lib_anim_callback_set_y, NULL);
     y_pos_now -= 20;
 }
 
@@ -45,49 +45,49 @@ static void _IdleMove2_Animation(void)
 {
     int16_t VerPanel_y_pos_now = 0;
     int16_t MainPanel_y_pos_now = -25;
-    lv_lib_anim_user_animation(ui_EyesVerMovePanel, 0, 500, VerPanel_y_pos_now, VerPanel_y_pos_now-20, 0, 0, 0, 0, lv_anim_path_ease_out, lv_lib_anim_callback_set_y);
+    lv_lib_anim_user_animation(ui_EyesVerMovePanel, 0, 500, VerPanel_y_pos_now, VerPanel_y_pos_now-20, 0, 0, 0, 0, lv_anim_path_ease_out, lv_lib_anim_callback_set_y, NULL);
     VerPanel_y_pos_now -= 20;
-    lv_lib_anim_user_animation(ui_EyesVerMovePanel, 2500, 500, VerPanel_y_pos_now, VerPanel_y_pos_now+20, 0, 0, 0, 0, lv_anim_path_ease_out, lv_lib_anim_callback_set_y);
+    lv_lib_anim_user_animation(ui_EyesVerMovePanel, 2500, 500, VerPanel_y_pos_now, VerPanel_y_pos_now+20, 0, 0, 0, 0, lv_anim_path_ease_out, lv_lib_anim_callback_set_y, NULL);
     VerPanel_y_pos_now += 20;
 
-    lv_lib_anim_user_animation(ui_EyesPanel, 0, 500, MainPanel_y_pos_now, MainPanel_y_pos_now+20, 0, 0, 0, 0, lv_anim_path_ease_out, lv_lib_anim_callback_set_y);
+    lv_lib_anim_user_animation(ui_EyesPanel, 0, 500, MainPanel_y_pos_now, MainPanel_y_pos_now+20, 0, 0, 0, 0, lv_anim_path_ease_out, lv_lib_anim_callback_set_y, NULL);
     MainPanel_y_pos_now += 20;
-    lv_lib_anim_user_animation(ui_EyesPanel, 2500, 500, MainPanel_y_pos_now, MainPanel_y_pos_now-20, 0, 0, 0, 0, lv_anim_path_ease_out, lv_lib_anim_callback_set_y);
+    lv_lib_anim_user_animation(ui_EyesPanel, 2500, 500, MainPanel_y_pos_now, MainPanel_y_pos_now-20, 0, 0, 0, 0, lv_anim_path_ease_out, lv_lib_anim_callback_set_y, NULL);
     MainPanel_y_pos_now -= 20;
 
-    lv_lib_anim_user_animation(ui_thinkImg, 0, 500, -100, 100, 0, 750, 0, 3, lv_anim_path_ease_in_out, lv_lib_anim_callback_set_image_angle);
-    lv_lib_anim_user_animation(ui_thinkImg, 0, 500, 0, 255, 0, 0, 0, 0, lv_anim_path_linear, lv_lib_anim_callback_set_opacity);
-    lv_lib_anim_user_animation(ui_thinkImg, 3000, 500, 255, 0, 0, 0, 0, 0, lv_anim_path_linear, lv_lib_anim_callback_set_opacity);
+    lv_lib_anim_user_animation(ui_thinkImg, 0, 500, -100, 100, 0, 750, 0, 3, lv_anim_path_ease_in_out, lv_lib_anim_callback_set_image_angle, NULL);
+    lv_lib_anim_user_animation(ui_thinkImg, 0, 500, 0, 255, 0, 0, 0, 0, lv_anim_path_linear, lv_lib_anim_callback_set_opacity, NULL);
+    lv_lib_anim_user_animation(ui_thinkImg, 3000, 500, 255, 0, 0, 0, 0, 0, lv_anim_path_linear, lv_lib_anim_callback_set_opacity, NULL);
 }
 
 static void _ListenMove_Animation(void)
 {
     int16_t eye_width_now = 80;
     int16_t eye_hight_now = 80;
-    lv_lib_anim_user_animation(ui_EyeRight, 0, 100, eye_width_now, eye_width_now-30, 0, 0, 0, 0, lv_anim_path_ease_out, lv_lib_anim_callback_set_width);
-    lv_lib_anim_user_animation(ui_EyeLeft, 0, 100, eye_width_now, eye_width_now-30, 0, 0, 0, 0, lv_anim_path_ease_out, lv_lib_anim_callback_set_width);
+    lv_lib_anim_user_animation(ui_EyeRight, 0, 100, eye_width_now, eye_width_now-30, 0, 0, 0, 0, lv_anim_path_ease_out, lv_lib_anim_callback_set_width, NULL);
+    lv_lib_anim_user_animation(ui_EyeLeft, 0, 100, eye_width_now, eye_width_now-30, 0, 0, 0, 0, lv_anim_path_ease_out, lv_lib_anim_callback_set_width, NULL);
     eye_width_now -= 30;
-    lv_lib_anim_user_animation(ui_QuestionImg, 0, 500, 0, 255, 0, 0, 0, 0, lv_anim_path_linear, lv_lib_anim_callback_set_opacity);
-    lv_lib_anim_user_animation(ui_QuestionImg, 0, 500, -100, 100, 0, 750, 0, 4, lv_anim_path_ease_in_out, lv_lib_anim_callback_set_image_angle);
+    lv_lib_anim_user_animation(ui_QuestionImg, 0, 500, 0, 255, 0, 0, 0, 0, lv_anim_path_linear, lv_lib_anim_callback_set_opacity, NULL);
+    lv_lib_anim_user_animation(ui_QuestionImg, 0, 500, -100, 100, 0, 750, 0, 4, lv_anim_path_ease_in_out, lv_lib_anim_callback_set_image_angle, NULL);
 
-    lv_lib_anim_user_animation(ui_EyeRight, 1000, 100, eye_width_now, eye_width_now+30, 0, 100, 0, 1, lv_anim_path_ease_out, lv_lib_anim_callback_set_width);
-    lv_lib_anim_user_animation(ui_EyeLeft, 1000, 100, eye_width_now, eye_width_now+30, 0, 100, 0, 1, lv_anim_path_ease_out, lv_lib_anim_callback_set_width);
-    lv_lib_anim_user_animation(ui_EyeRight, 1000, 100, eye_hight_now, eye_hight_now-70, 0, 100, 0, 1, lv_anim_path_ease_out, lv_lib_anim_callback_set_hight);
-    lv_lib_anim_user_animation(ui_EyeLeft, 1000, 100, eye_hight_now, eye_hight_now-70, 0, 100, 0, 1, lv_anim_path_ease_out, lv_lib_anim_callback_set_hight);
+    lv_lib_anim_user_animation(ui_EyeRight, 1000, 100, eye_width_now, eye_width_now+30, 0, 100, 0, 1, lv_anim_path_ease_out, lv_lib_anim_callback_set_width, NULL);
+    lv_lib_anim_user_animation(ui_EyeLeft, 1000, 100, eye_width_now, eye_width_now+30, 0, 100, 0, 1, lv_anim_path_ease_out, lv_lib_anim_callback_set_width, NULL);
+    lv_lib_anim_user_animation(ui_EyeRight, 1000, 100, eye_hight_now, eye_hight_now-70, 0, 100, 0, 1, lv_anim_path_ease_out, lv_lib_anim_callback_set_hight, NULL);
+    lv_lib_anim_user_animation(ui_EyeLeft, 1000, 100, eye_hight_now, eye_hight_now-70, 0, 100, 0, 1, lv_anim_path_ease_out, lv_lib_anim_callback_set_hight, NULL);
 
-    lv_lib_anim_user_animation(ui_EyeRight, 2000, 100, eye_width_now, eye_width_now+30, 0, 100, 0, 1, lv_anim_path_ease_out, lv_lib_anim_callback_set_width);
-    lv_lib_anim_user_animation(ui_EyeLeft, 2000, 100, eye_width_now, eye_width_now+30, 0, 100, 0, 1, lv_anim_path_ease_out, lv_lib_anim_callback_set_width);
-    lv_lib_anim_user_animation(ui_EyeRight, 2000, 100, eye_hight_now, eye_hight_now-70, 0, 100, 0, 1, lv_anim_path_ease_out, lv_lib_anim_callback_set_hight);
-    lv_lib_anim_user_animation(ui_EyeLeft, 2000, 100, eye_hight_now, eye_hight_now-70, 0, 100, 0, 1, lv_anim_path_ease_out, lv_lib_anim_callback_set_hight);
+    lv_lib_anim_user_animation(ui_EyeRight, 2000, 100, eye_width_now, eye_width_now+30, 0, 100, 0, 1, lv_anim_path_ease_out, lv_lib_anim_callback_set_width, NULL);
+    lv_lib_anim_user_animation(ui_EyeLeft, 2000, 100, eye_width_now, eye_width_now+30, 0, 100, 0, 1, lv_anim_path_ease_out, lv_lib_anim_callback_set_width, NULL);
+    lv_lib_anim_user_animation(ui_EyeRight, 2000, 100, eye_hight_now, eye_hight_now-70, 0, 100, 0, 1, lv_anim_path_ease_out, lv_lib_anim_callback_set_hight, NULL);
+    lv_lib_anim_user_animation(ui_EyeLeft, 2000, 100, eye_hight_now, eye_hight_now-70, 0, 100, 0, 1, lv_anim_path_ease_out, lv_lib_anim_callback_set_hight, NULL);
 
-    lv_lib_anim_user_animation(ui_HandImg, 2000, 500, 0, 255, 0, 0, 0, 0, lv_anim_path_linear, lv_lib_anim_callback_set_opacity);
-    lv_lib_anim_user_animation(ui_HandImg, 2000, 500, -250, -150, 0, 750, 0, 2, lv_anim_path_ease_in_out, lv_lib_anim_callback_set_image_angle);
+    lv_lib_anim_user_animation(ui_HandImg, 2000, 500, 0, 255, 0, 0, 0, 0, lv_anim_path_linear, lv_lib_anim_callback_set_opacity, NULL);
+    lv_lib_anim_user_animation(ui_HandImg, 2000, 500, -250, -150, 0, 750, 0, 2, lv_anim_path_ease_in_out, lv_lib_anim_callback_set_image_angle, NULL);
 
-    lv_lib_anim_user_animation(ui_QuestionImg, 3500, 500, 255, 0, 0, 0, 0, 0, lv_anim_path_linear, lv_lib_anim_callback_set_opacity);
-    lv_lib_anim_user_animation(ui_HandImg, 3500, 500, 255, 0, 0, 0, 0, 0, lv_anim_path_linear, lv_lib_anim_callback_set_opacity);
+    lv_lib_anim_user_animation(ui_QuestionImg, 3500, 500, 255, 0, 0, 0, 0, 0, lv_anim_path_linear, lv_lib_anim_callback_set_opacity, NULL);
+    lv_lib_anim_user_animation(ui_HandImg, 3500, 500, 255, 0, 0, 0, 0, 0, lv_anim_path_linear, lv_lib_anim_callback_set_opacity, NULL);
 
-    lv_lib_anim_user_animation(ui_EyeRight, 4000, 100, eye_width_now, eye_width_now+30, 0, 0, 0, 0, lv_anim_path_ease_out, lv_lib_anim_callback_set_width);
-    lv_lib_anim_user_animation(ui_EyeLeft, 4000, 100, eye_width_now, eye_width_now+30, 0, 0, 0, 0, lv_anim_path_ease_out, lv_lib_anim_callback_set_width);
+    lv_lib_anim_user_animation(ui_EyeRight, 4000, 100, eye_width_now, eye_width_now+30, 0, 0, 0, 0, lv_anim_path_ease_out, lv_lib_anim_callback_set_width, NULL);
+    lv_lib_anim_user_animation(ui_EyeLeft, 4000, 100, eye_width_now, eye_width_now+30, 0, 0, 0, 0, lv_anim_path_ease_out, lv_lib_anim_callback_set_width, NULL);
     eye_width_now += 30;
 }
 
@@ -97,14 +97,14 @@ static void _SpeakMove_Animation(void)
     int16_t mouth_y_pos_now = -40;
     int16_t eye_panel_hight_now = 80;
     lv_obj_set_style_bg_opa(ui_Mouth, 255, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_lib_anim_user_animation(ui_Mouth, 0, 150, mouth_y_pos_now, mouth_y_pos_now-10, 0, 150, 0, 6, lv_anim_path_ease_out, lv_lib_anim_callback_set_y);
-    lv_lib_anim_user_animation(ui_MouthPanel, 0, 150, mouth_panel_y_pos_now, mouth_panel_y_pos_now+10, 0, 150, 0, 6, lv_anim_path_ease_out, lv_lib_anim_callback_set_y);
+    lv_lib_anim_user_animation(ui_Mouth, 0, 150, mouth_y_pos_now, mouth_y_pos_now-10, 0, 150, 0, 6, lv_anim_path_ease_out, lv_lib_anim_callback_set_y, NULL);
+    lv_lib_anim_user_animation(ui_MouthPanel, 0, 150, mouth_panel_y_pos_now, mouth_panel_y_pos_now+10, 0, 150, 0, 6, lv_anim_path_ease_out, lv_lib_anim_callback_set_y, NULL);
     // blink
-    lv_lib_anim_user_animation(ui_EyesPanel, 500, 200, eye_panel_hight_now, 10, 0, 200, 0, 1, lv_anim_path_ease_in_out, lv_lib_anim_callback_set_hight);
-    lv_lib_anim_user_animation(ui_EyesPanel, 2000, 200, eye_panel_hight_now, 10, 0, 200, 0, 1, lv_anim_path_ease_in_out, lv_lib_anim_callback_set_hight);
+    lv_lib_anim_user_animation(ui_EyesPanel, 500, 200, eye_panel_hight_now, 10, 0, 200, 0, 1, lv_anim_path_ease_in_out, lv_lib_anim_callback_set_hight, NULL);
+    lv_lib_anim_user_animation(ui_EyesPanel, 2000, 200, eye_panel_hight_now, 10, 0, 200, 0, 1, lv_anim_path_ease_in_out, lv_lib_anim_callback_set_hight, NULL);
 
-    lv_lib_anim_user_animation(ui_Mouth, 1500, 150, mouth_y_pos_now, mouth_y_pos_now-10, 0, 150, 0, 4, lv_anim_path_ease_out, lv_lib_anim_callback_set_y);
-    lv_lib_anim_user_animation(ui_MouthPanel, 1500, 150, mouth_panel_y_pos_now, mouth_panel_y_pos_now+10, 0, 150, 0, 4, lv_anim_path_ease_out, lv_lib_anim_callback_set_y);
+    lv_lib_anim_user_animation(ui_Mouth, 1500, 150, mouth_y_pos_now, mouth_y_pos_now-10, 0, 150, 0, 4, lv_anim_path_ease_out, lv_lib_anim_callback_set_y, NULL);
+    lv_lib_anim_user_animation(ui_MouthPanel, 1500, 150, mouth_panel_y_pos_now, mouth_panel_y_pos_now+10, 0, 150, 0, 4, lv_anim_path_ease_out, lv_lib_anim_callback_set_y, NULL);
 
 }
 
