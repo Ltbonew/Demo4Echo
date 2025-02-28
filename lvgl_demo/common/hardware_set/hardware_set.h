cd3c0c975528fb33fc6883b0e2eb17447c75550f
kingham <1425962791@qq.com>
2025-02-28 11:29:06 +0800

    add hardware_set

---
+++ lvgl_demo/common/hardware_set/hardware_set.h
@@ -0,0 +1,14 @@
+#ifndef _HARDWARE_SET_H
+#define _HARDWARE_SET_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "../../conf/dev_conf.h"
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif
