dbff478bb592183be07e4c0afb9b387a2f64f226
kingham <1425962791@qq.com>
2025-05-23 15:47:20 +0800

    change log

--- AIChat_demo/Client_V2/Application/UserIntents/RobotMove.cc
+++ AIChat_demo/Client_V2/Application/UserIntents/RobotMove.cc
@@ -1,15 +1,16 @@
 // RobotMove.cc
 #include "RobotMove.h"
 #include <iostream>
+#include "../../Utils/user_log.h"
 
 namespace RobotMove {
     void Move(const Json::Value& arguments) {
         // 从 arguments 中获取 "direction" 参数
         if (arguments.isMember("direction") && arguments["direction"].isString()) {
             std::string direction = arguments["direction"].asString();
-            std::cout << "Robot moving " << direction << std::endl;
+            USER_LOG_INFO("RobotMove::Move direction: %s", direction.c_str());
         } else {
-            std::cerr << "Invalid or missing 'direction' argument in RobotMove::Move" << std::endl;
+            USER_LOG_INFO("Invalid or missing 'direction' argument in RobotMove::Move");
         }
     }
 }
